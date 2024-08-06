be4e409b9bd0ba22f4f3cf51456e016a166b4d5b
asisj <asisj@DESKTOP-RR0HP8G>
2024-08-06 20:29:23 +0530

    Add project files.

---
+++ EasyBuy/Program.cs
@@ -0,0 +1,24 @@
+﻿using EasyBuy.Forms.Cashier;
+using EasyBuy.Forms.Nexus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyBuy
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Splash_Screen());
+        }
+    }
+}
