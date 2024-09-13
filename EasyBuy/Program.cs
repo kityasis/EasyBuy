8a910a2e7417af2767a18e6a29d763b4c77d7eac
Asis Jena <asisjena03@hotmail.com>
2024-09-13 12:42:59 +0530

    fix

--- EasyBuy/Program.cs
+++ EasyBuy/Program.cs
@@ -21,7 +21,7 @@ namespace EasyBuy
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Cashier());
+            Application.Run(new Home());
         }
     }
 }
